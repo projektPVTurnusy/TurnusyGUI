4e49444acac3bc610a1a007c084e9e5e2d4c4938
MichalMou <ondas179@gmail.com>
2023-05-04 15:12:29 +0200

    Basic

--- src/GUI/WindowGUI.java
+++ src/GUI/WindowGUI.java
@@ -1,33 +1,93 @@
 package GUI;
 
-import Main.Main;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class WindowGUI extends javax.swing.JFrame {
 
-    private Main main;
+    private boolean addingEdge;
+    private boolean addingNode;
+    private boolean addingMode;
+
     private JTabbedPane tabbedPane1;
     private JPanel JPanelDraw;
     private JButton loadDataButton;
     private JButton saveDataButton;
     private JPanel JPanelToolMenu;
+    private JButton addEdgeButton;
+    private JButton AddNodeButton;
+    private JPanel mainPanel;
+    private JPanel JPanelInfo;
 
-    public WindowGUI(Main main) {
-        this.main = main;
-        this.setVisible(true);
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setMinimumSize(new java.awt.Dimension(800, 500));
-        setPreferredSize(new java.awt.Dimension(800, 500));
-        setBackground(new Color(91, 91, 91));
+    public WindowGUI() {
+        setTitle("Drawing system");
+        setMinimumSize(new Dimension(800,500));
+        pack();
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainPanel.setVisible(true);
+        add(mainPanel);
+
 
         this.JPanelDraw = new Canvas();
+        this.addingEdge = false;
+        this.addingNode = false;
+        this.addingMode = false;
 
-        JPanelToolMenu.setSize(50,800);
-        JPanelToolMenu.setBackground(new Color(90,90,90));
 
+        addEdgeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (addingNode) {
+                    addingNode = false;
+                }
+
+                if (addingEdge) {
+                    addingEdge = false;
+                    addingMode = false;
+
+                } else {
+                    addingEdge = true;
+                    addingMode = true;
+                }
+                changeCursor();
+                // TODO vytvaranie Node
+
+
+            }
+        });
+        AddNodeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (addingEdge) {
+                    addingEdge = false;
+                }
+
+                if (addingNode) {
+                    addingNode = false;
+                    addingMode = false;
+
+                } else {
+                    addingNode = true;
+                    addingMode = true;
+                }
+                changeCursor();
+                // TODO vytvaranie Edge
+            }
+        });
     };
 
 
+    /*
+    * Zmena cursora nech je viditelne ze sa vytvara daky objekt napr node alebo edge
+    * */
+    private void changeCursor() {
+        if (this.addingMode) {
+            setCursor(Cursor.getDefaultCursor());
+        } else {
+            setCursor( Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
+        }
+    }
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -8,20 +8,11 @@ import GUI.WindowGUI;
 
 
 public class Main {
-    private WindowGUI gui;
     private DataManager dataManager;
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        new Main();
-
+        WindowGUI windowGUI = new WindowGUI();
     }
 
-    private Main() {
-        this.gui = new WindowGUI(this);
-
-    }
 }
